fa3eeee70c794982afdda4e3de58ebfb0ab0234a
Luong Vinh Loi <luongvinhloi_t65@hus.edu.vn>
2024-09-03 14:15:13 +0700

    video 70

--- src/main/java/vn/hoidanit/jobhunter/domain/Company.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Company.java
@@ -1,15 +1,20 @@
 package vn.hoidanit.jobhunter.domain;
 
 import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import vn.hoidanit.jobhunter.util.SecurityUtil;
 
 @Entity
 @Table(name = "companies")
@@ -26,10 +31,17 @@ public class Company {
     @Column(columnDefinition = "MEDIUMTEXT")
     private String description;
     private String address;
-    private String logo; 
+    private String logo;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss a", timezone = "GMT+7")
     private Instant createdAt;
     private Instant updatedAt;
     private String createdBy;
     private String updatedBy;
+
+    @PrePersist
+    public void handleCreateBy() {
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() ? SecurityUtil.getCurrentUserLogin().get() : "";
+        this.createdAt = Instant.now();
+    }
 
 }
