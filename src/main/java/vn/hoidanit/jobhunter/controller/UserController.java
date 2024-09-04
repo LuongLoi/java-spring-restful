e9b7c5f1af18dfe64f51e5452e8996dd1ae6f895
Luong Vinh Loi <luongvinhloi_t65@hus.edu.vn>
2024-09-04 22:17:12 +0700

    video 76 filter with specification

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -3,6 +3,7 @@ import java.util.Optional;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.turkraft.springfilter.boot.Filter;
 
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.domain.dto.ResultPaginationDTO;
@@ -56,15 +59,9 @@ public class UserController {
 
     @GetMapping("/users")
     public ResponseEntity<ResultPaginationDTO> getAllUsers(
-       @RequestParam("current") Optional<String> currentOptional,
-       @RequestParam("pageSize") Optional<String> pageSizeOptional
+        @Filter Specification<User> spec, Pageable pageable
     ) {
-        String sCurrent = currentOptional.isPresent() ? currentOptional.get() : "";
-        String sPageSize = pageSizeOptional.isPresent() ? pageSizeOptional.get() : "";
-        int current = Integer.parseInt(sCurrent);
-        int pageSize = Integer.parseInt(sPageSize);
-        Pageable pageable = PageRequest.of(current - 1, pageSize);
-        return ResponseEntity.ok(this.userService.handleGetAllUsers(pageable));
+        return ResponseEntity.ok(this.userService.handleGetAllUsers(spec,pageable));
     }
 
     @PutMapping("/users")
--- src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
@@ -1,9 +1,8 @@
 package vn.hoidanit.jobhunter.service;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.Company;
@@ -24,12 +23,12 @@ public class CompanyService {
         return this.companyRepository.save(company);
     }
 
-    public ResultPaginationDTO handleGetAllCompany(Pageable pageable) {
-        Page<Company> pageCompany = this.companyRepository.findAll(pageable);
+    public ResultPaginationDTO handleGetAllCompany(Specification<Company> spec ,Pageable pageable) {
+        Page<Company> pageCompany = this.companyRepository.findAll(spec,pageable);
         ResultPaginationDTO rs = new ResultPaginationDTO();
         Meta mt = new Meta();
         
-        mt.setPage(pageCompany.getNumber());
+        mt.setPage(pageCompany.getNumber() + 1);
         mt.setPageSize(pageCompany.getSize());
         mt.setPages(pageCompany.getTotalPages());
         mt.setTotal(pageCompany.getTotalElements());
