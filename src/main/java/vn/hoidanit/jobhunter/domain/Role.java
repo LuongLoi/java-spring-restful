08ef0196760838aa2e1f6e61bc8dd7080acafde9
Luong Vinh Loi <luongvinhloi_t65@hus.edu.vn>
2024-10-29 22:35:58 +0700

    video 108

---
+++ src/main/java/vn/hoidanit/jobhunter/domain/Role.java
@@ -0,0 +1,68 @@
+package vn.hoidanit.jobhunter.domain;
+
+import java.time.Instant;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import vn.hoidanit.jobhunter.util.SecurityUtil;
+
+@Table(name = "roles")
+@Entity
+@Getter
+@Setter
+public class Role {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Tên role không được để trống!")
+    private String name;
+
+    private String description;
+
+    private boolean active;
+
+    private Instant createdAt;
+
+    private String createdBy;
+
+    private Instant updatedAt;
+
+    private String updatedBy;
+    
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(value = { "roles" })
+    @JoinTable(name = "permission_role", 
+            joinColumns = @JoinColumn(name = "permission_id"), 
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Permission> permissions;
+
+    @PrePersist
+    public void handleBeforeCreate() {
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent() == true ? SecurityUtil.getCurrentUserLogin().get() : "";
+        this.createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void handleAfterUpdate() {
+        this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent() == true ? SecurityUtil.getCurrentUserLogin().get() : "";
+        this.updatedAt = Instant.now();
+    }
+
+}
